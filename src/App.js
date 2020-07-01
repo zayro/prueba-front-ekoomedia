bab176df89520db15c9e8c7f632992ab300c955e
marlon zayro <zayro8905@gmail.com>
2020-07-01 12:04:24 -0500

    agregar informacion por post

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import swal from 'sweetalert';
 import './style/menu.scss';
 
 function App() {
 
-    // Declara una nueva variable de estado, la cual llamaremos “count”
+    // Declara title del texto
     const [title, setTitle] = useState('X');
+
+    const [menu, setMenu] = useState({ data: [] });
 
     const [datos, setDatos] = useState({
         nombre: '',
-        apellido: ''
+        email: '',
+        celular: '',
+        edad: ''
     })
 
     const handleInputChange = (event) => {
@@ -16,45 +22,104 @@ function App() {
         // console.log(event.target.value)
         setDatos({
             ...datos,
-            [event.target.name] : event.target.value
+            [event.target.name]: event.target.value
         })
     }
 
-    const enviarDatos = (event) => {
+    const fetchDataMenu = async () => {
+        const result = await axios(
+            `http://localhost:8000/ObtenerMenu`,
+        );
+
+        console.log('result', result.data.data);
+        console.log('result', result.data.status);
+
+        if (result.data.status) {
+            setMenu(result.data);
+        }
+
+
+    };
+
+    const GuardarInformacion = (event) => {
+
         event.preventDefault()
-        console.log('enviando datos...' + datos.nombre + ' ' + datos.apellido)
-    }
+
+        axios.request({
+            method: 'post',
+            url: `http://localhost:8000/GuardaInformacion`,
+            data: datos,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+              },        }
+
+            , )
+          .then(function (response) {
+            console.log(response);
+            console.log(response.message);
+            swal({
+                title: "Exitoso!",
+                text: `${response.data.message}`,
+                icon: "success",
+                button: "cerrar",
+                timer: 3000
+              });
+          })
+          .catch(function (error) {
+            console.log(error);
+            swal({
+                title: "opps ",
+                text: `ocurrio un problema!`,
+                icon: "error",
+                button: "cerrar",
+                timer: 3000
+              });
+          });
+
+
+    };
+
+
+    useEffect(() => {
+        fetchDataMenu();
+
+
+    }, []);
 
 
     return (
 
         <div className="App">
 
-
             <header>
+
+
 
                 <nav>
                     <ul className="sidenav">
-                        <li><a className="active" href="#home">Home</a></li>
-                        <li><a href="#news">News</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li><a href="#about" onClick={() => setTitle('Abot')} > About</a></li>
+                        {menu.data.map(item => (
+                            <li key={item.id}>
+                                <a className={`${title === item.nombre ? 'active' : ''}`} onClick={() => setTitle(item.nombre)} href={'#' + item.nombre}>{item.nombre}</a>
+                            </li>
+                        ))}
+
                     </ul>
                 </nav>
 
                 <h1>Hola bienvenido sabemos que quieres viajar en un {title}  </h1>
 
-                <hr/>
+                <hr />
 
 
-                <form onSubmit={enviarDatos}>
+                <form onSubmit={GuardarInformacion}>
 
-                <input type="text" placeholder="nombre" onChange={handleInputChange} name="nombre" type="text" required />
-                <input type="text" placeholder="email"  onChange={handleInputChange} name="email" type="email" />
-                <input type="text" placeholder="celular"  onChange={handleInputChange} name="celular" type="text" />
-                <input type="text" placeholder="number"  onChange={handleInputChange} name="edad" type="number" />
+                    <input type="text" placeholder="nombre" onChange={handleInputChange} name="nombre" type="text" required />
+                    <input type="text" placeholder="email" onChange={handleInputChange} name="email" type="email" />
+                    <input type="text" placeholder="celular" onChange={handleInputChange} name="celular" type="text" />
+                    <input type="text" placeholder="number" onChange={handleInputChange} name="edad" type="number" />
 
-                <input type="submit" value="Submit" />
+                    <input type="submit" value="Submit" />
                 </form>
 
 
